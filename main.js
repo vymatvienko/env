8da6c741d88884002040fc022f92207d7f8ab1d5
AppVeyor <ci@appveyor.com>
2023-09-24 16:44:28 +0000

    2210c88

---
+++ main.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/app.js
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+function demo(value) {
+  return `Demo: ${value}`;
+}
+console.log("app.js included");
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+// TODO: write your code in app.js
+/******/ })()
+;
